7962166974b472a390dc06d68e2ab563c0bdc8fe
yszhou2016 <416256889@qq.com>
2022-10-25 19:35:30 +0800

    Update 3.Indel_Caculate.pl

--- 3.Indel_Caculate.pl
+++ 3.Indel_Caculate.pl
@@ -22,52 +22,66 @@ else{ $spacer=~tr/ATCG/TAGC/;  $spacer=reverse($spacer);
 
 
 open(BB,"$filein.sam");   
-open(C1,">temp");
-
-print  C1 $reference,"\n";
-if($bad_input==0){print  C1 "\ "x($start-8),"Target: ","*"x$len,"\n";}
- else{print C1 "Wrong input target sequences!";}
-
-$Edit=0;  $total=0;  $Deletion=0;  $Insertion=0;
+open(C1,">temp1");
+open(C2,">temp2");
+$Edit=0;  $Match=0;  $Deletion=0;  $Insertion=0;  $to=0;
 while($line=<BB>)
 {     chomp $line;
       @bb=split/\s+/,$line;
-      $ll=$bb[5];   $ll2=$bb[5];    $D=0;   $I=0;  $D2=0;  $I2=0; $overlap=0;
+      $ll=$bb[5];   $ll2=$bb[5];    $D=0;   $I=0;  $D2=0;  $I2=0; $overlap=0;  
       $ll=~s/M/\t/g;  $ll=~s/D/\t/g; $ll=~s/I/\t/g;
       $ll2=~s/M/M\t/g;  $ll2=~s/D/D\t/g; $ll2=~s/I/I\t/g;
       @ar=split/\s+/,$ll;    @ar2=split/\s+/,$ll2;
       $lenth=@ar;  
-      if($bb[3]>0 && $bb[0]!=~/^@/){$total++;}
+      if($bb[3]>1 && $bb[0]!=~/^@/){$to++;   
       if($lenth>1){
           $seq0="_"x($bb[3]-1);  
-          $i=0;  $ss="";  
-       $sum="$seq0";
-       while($i<$lenth){   
-       if($ar2[$i]=~/M$/){   $seq[$i]=substr($bb[9],$ss,$ar[$i]);    $ss=$ss+$ar[$i];  } 
-       elsif($ar2[$i]=~/D$/){   $seq[$i]="-"x$ar[$i];  if(($ss+$bb[3]-1)>($end+10) || ($ss+$ar[$i]+$bb[3]-1)<$start){}else{$overlap=1; $D2=1;   $D=$D+$ar[$i];}  }    
-       elsif($ar2[$i]=~/I$/){   $seq[$i]=substr($bb[9],$ss,$ar[$i]);  $seq[$i]=~tr/ATGC/atgc/;  
+          $i=0;  $ss=0;  
+             $sum="$seq0";
+          while($i<$lenth){   
+               if($ar2[$i]=~/M$/){   $seq[$i]=substr($bb[9],$ss,$ar[$i]);    $ss=$ss+$ar[$i];  } 
+               elsif($ar2[$i]=~/D$/){   $seq[$i]="-"x$ar[$i];  if(($ss+$bb[3]-1)>($end+10) || ($ss+$ar[$i]+$bb[3]-1)<$start){}else{$overlap=1; $D2=1;   $D=$D+$ar[$i];}  }    
+               elsif($ar2[$i]=~/I$/){   $seq[$i]=substr($bb[9],$ss,$ar[$i]);  $seq[$i]=~tr/ATGC/atgc/;  
            if(($ss+$bb[3]-1)>($end+10) || ($ss+$ar[$i]+$bb[3]-1)<$start){}else{$overlap=1; $I2=1; $I=$I+$ar[$i]; }    $ss=$ss+$ar[$i]; }    
-         $sum="$sum$seq[$i]";
+             $sum="$sum$seq[$i]";
            $i++;  
-        }
-          if($overlap>0){  $Edit++;  
-          if($D2>0){$Deletion++;}
-          if($I2>0){$Insertion++;}
-          print  C1  "$sum\tDel_len:$D\tIns_len:$I","\n";
-         }
-      }
+           }
+             if($overlap>0){  $Edit++;  
+                        if($D2>0){$Deletion++;}
+                        if($I2>0){$Insertion++;}
+                 $string="$sum\tDel_len:$D\tIns_len:$I";    #$string=substr($string,$start-20,300);
+                        print  C1  $string,"\n";
+                 if(not exists $v{$string}){  $v{$string}=1;  }else{  $v{$string}++;  }
+                      }
+      }else{     
+                                     if($ar2[0]=~/M$/ &&   $ar[0]>100){
+                                        $seq0="_"x($bb[3]-1);
+                                          $seq1=substr($bb[9],0,$ar[0]);      
+                                                    $string="$seq0$seq1\tDel_len:0\tIns_len:0";    
+                           if(not exists $v{$string}){ $v{$string}=1; }else{ $v{$string}++; }   
+                                        print  C2  $string,"\n";    
+                                         $Match++;   
+                                     }
+               }
+           }
 }
-$rate=int(10000*$Edit/($total+1))/100;
-close BB; close C1;
+$total=$Edit+$Match;
+$RateE=int(10000*$Edit/($total+1))/100;
+$RateW=int(100*(100-$RateE))/100;
+close BB; close C1;  close C2;
+
+open(AA,"temp1");   open(BB,">temp11");  while($line=<AA>){chomp $line;  @bb=split/\s+/,$line;     $rate=int(10000*$v{$line}/$total)/100;  if(not exists($vv{$line})){print BB $bb[0],"\t","Rate: $rate (%)\t","Count:$v{$line}","\t$bb[1]\t$bb[2]\n";  $vv{$line}=1;  }  }  close AA; close BB;
+open(AA,"temp2");   open(BB,">temp22");  while($line=<AA>){chomp $line;  @bb=split/\s+/,$line;       $rate=int(10000*$v{$line}/$total)/100;   if(not exists($vv{$line})){print BB $bb[0],"\t","Rate: $rate (%)\t","Count:$v{$line}","\t$bb[1]\t$bb[2]\n";  $vv{$line}=1;  }  }  close AA; close BB;
+`sort  -dk3,3nr  temp11  >  temp11.sorted`;
+`sort  -dk3,3nr  temp22  >  temp22.sorted`;
+open(AA,"temp22.sorted");  $line=<AA>;   @bb=split/\s+/,$line;  $WT=$bb[0];  close AA;
+
 open(FF,">$fileout");
-print FF "Editing_rate: $rate %\t","Editing_Reads: $Edit\t","Total_Reads: $total","\t","Deletion_Reads: $Deletion","\t","Insertion_Reads: $Insertion","\n\n>reference\n";
+     print FF "Editing_rate: $RateE %\t","Editing_Reads: $Edit\t","Total_Reads: $total","\t","Deletion_Reads: $Deletion","\t","Insertion_Reads: $Insertion","\n\n>reference\n$reference\n";
+    if($bad_input==0){print FF "\ "x($start-8),"Target: ","*"x$len,"\n";}
+    else{print FF "Wrong input target sequences!";}
+    print FF "$WT\tRate: $RateW (%)\t","Count:$Match      Del_len:0       Ins_len:0\n";  
 close FF;
-open(FF,">$fileout.result");
-print FF "$filein\tEditing_rate: $rate %\t","Editing_Reads: $Edit\t","Total_Reads: $total","\t","Deletion_Reads: $Deletion","\t","Insertion_Reads: $Insertion","\n\n>reference\n";
-close FF;
-open(FF,">$fileout.result");
-print FF "$filein\tEditing_rate: $rate %\t","Editing_Reads: $Edit\t","Total_Reads: $total","\t","Deletion_Reads: $Deletion","\t","Insertion_Reads: $Insertion","\n";
-close FF;
-`cat temp >> $fileout`;
-`rm temp`;
-`rm $file.sam`;
+`cat temp11.sorted >> $fileout`;
+`rm temp*`;
+`rm $filein.sam`;
